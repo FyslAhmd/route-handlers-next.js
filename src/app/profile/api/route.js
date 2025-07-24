8ecdd9ecb9a76ea37651d3599672a76e81f103ef
Faysal Ahmed <info.faysal.32@gmail.com>
2025-07-25 03:06:17 +0600

    explore different type of headers in next.js and way to access headers in server

--- src/app/profile/api/route.js
+++ src/app/profile/api/route.js
@@ -1,3 +1,12 @@
-export async function GET() {
-  return new Response("Profile Api");
+import { headers } from "next/headers";
+
+export async function GET(req) {
+  // const reqHeaders = new Headers(req.headers);
+  // console.log(reqHeaders.get("Authorization"));
+  const headerList = await headers();
+  console.log(headerList.get("Authorization"));
+
+  return new Response("<h1>Profile Api</h1>", {
+    headers: { "content-type": "text/html" },
+  });
 }
