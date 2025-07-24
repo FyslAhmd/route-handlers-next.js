08ce143388ed0eb4b80dc34e29bb284a7a83fa05
Faysal Ahmed <info.faysal.32@gmail.com>
2025-07-25 01:37:19 +0600

    add patch in the route to update data

--- src/app/comments/[id]/route.js
+++ src/app/comments/[id]/route.js
@@ -1,7 +1,15 @@
 import { comments } from "../data";
 
-export async function GET(req, { params }) {
+export async function GET(_req, { params }) {
   const { id } = await params;
   const comment = comments.find((com) => com.id === parseInt(id));
   return Response.json(comment);
+}
+
+export async function PATCH(req, { params }) {
+  const { id } = await params;
+  const { text } = await req.json();
+  const index = comments.findIndex((com) => com.id === parseInt(id));
+  comments[index].text = text;
+  return Response.json(comments[index]);
 }
