be7598fb1f59c9a482bbf093678c81af319bd4af
Faysal Ahmed <info.faysal.32@gmail.com>
2025-07-25 03:17:05 +0600

    cookies in route handlers

--- src/app/profile/api/route.js
+++ src/app/profile/api/route.js
@@ -1,4 +1,4 @@
-import { headers } from "next/headers";
+import { cookies, headers } from "next/headers";
 
 export async function GET(req) {
   // const reqHeaders = new Headers(req.headers);
@@ -6,7 +6,14 @@ export async function GET(req) {
   const headerList = await headers();
   console.log(headerList.get("Authorization"));
 
+  const theme = req.cookies.get("theme");
+  console.log(theme);
+
+  const cookieStore = await cookies();
+  cookieStore.set("resultPerPage", "20");
+  console.log(cookieStore.get("resultPerPage"));
+
   return new Response("<h1>Profile Api</h1>", {
-    headers: { "content-type": "text/html" },
+    headers: { "content-type": "text/html", "set-cookie": "theme=dark" },
   });
 }
